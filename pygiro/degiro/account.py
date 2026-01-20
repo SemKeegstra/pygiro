156785cd62dc9e3606161927a8aed26315dd47e7
SemKeegstra <sem_keegstra@live.nl>
2026-01-20 12:13:22 +0100

    added line types to statement

--- pygiro/degiro/account.py
+++ pygiro/degiro/account.py
@@ -2,7 +2,46 @@
 import pandas as pd
 
 # Constants:
-from ..utils.constants import STATEMENT_COLS, NUMERIC_COLS
+from ..utils.constants import *
+
+def _classify_line(line: pd.Series) -> str:
+    """
+    Classifies a single account statement line based on its description.
+
+    Parameters
+    ----------
+    line : str
+        Line from the account statement.
+
+    Returns
+    -------
+    str
+        Line type identifier (e.g. buy, sell, deposit, cost).
+    """
+    # Initialize:
+    description = str(line.description).lower()
+
+    # Classification:
+    if any(txt in description for txt in DEPOSITS):
+        return "deposit"
+    elif any(txt in description for txt in WITHDRAWALS):
+        return "withdrawal"
+    elif any(txt in description for txt in FX):
+        return "fx"
+    elif any(txt in description for txt in COSTS):
+        return "cost"
+    elif any(txt in description for txt in DIVIDENDS):
+        return "dividend"
+    elif any(txt in description for txt in INTEREST):
+        return "interest"
+    elif any(txt in description for txt in REBATE):
+        return "rebate"
+    elif any(txt in description for txt in SELL):
+        return "sell"
+    elif any(txt in description for txt in BUY):
+        return "buy"
+
+    return "other"
 
 
 def _import_account_statement(path: str) -> pd.DataFrame:
@@ -11,7 +50,7 @@ def _import_account_statement(path: str) -> pd.DataFrame:
 
     Notes
     -----
-    1. Repairs split rows, parses numeric fields and formats timestamps.
+    1. Repairs split rows, parses numeric fields, formats timestamps, and classifies line types.
 
     Parameters
     ----------
@@ -27,8 +66,8 @@ def _import_account_statement(path: str) -> pd.DataFrame:
     statement = pd.read_csv(path, sep=",", engine="c").set_axis(STATEMENT_COLS, axis=1)
 
     # Handle extended rows:
-    for idx, row in statement[statement.date.isna()].iterrows():
-        statement.iloc[idx-1] += row.fillna("")
+    for idx, line in statement[statement.date.isna()].iterrows():
+        statement.iloc[idx-1] += line.fillna("")
     statement.dropna(subset=["date"], inplace=True)
 
     # Date formatting:
@@ -37,5 +76,8 @@ def _import_account_statement(path: str) -> pd.DataFrame:
 
     # Number parsing:
     statement[NUMERIC_COLS] = statement[NUMERIC_COLS].replace({",":"."}, regex=True).astype(float)
+
+    # Type classification:
+    statement["type"] = pd.Categorical(statement.apply(_classify_line, axis=1), categories=LINE_TYPES)
 
     return statement
--- pygiro/utils/constants.py
+++ pygiro/utils/constants.py
@@ -1,3 +1,14 @@
-# Data Input Related:
-STATEMENT_COLS = ["date", "time", "currency_date", "name", "ISIN", "description", "FX", "mutation", "amount", "currency", "balance", "order_id"]
-NUMERIC_COLS = ["FX", "amount", "balance"]
+# Account Statement Related:
+STATEMENT_COLS = ['date', 'time', 'currency_date', 'name', 'ISIN', 'description', 'FX', 'mutation', 'amount', 'currency', 'balance', 'order_id']
+NUMERIC_COLS = ['FX', 'amount', 'balance']
+
+LINE_TYPES = ['deposit', 'withdrawal', 'fx', 'cost', 'dividend', 'interest', 'rebate', 'sell', 'buy', 'other']
+DEPOSITS = {'ideal deposit', 'sofort deposit'}
+WITHDRAWALS = {'ideal withdrawal', 'sofort withdrawal'}
+FX = {'valuta debitering', 'valuta creditering'}
+SELL = {'verkoop'}
+BUY = {'koop'}
+DIVIDENDS = {'dividend'}
+INTEREST = {'flatex interest income'}
+REBATE = {'verrekening promotie'}
+COSTS = {'degiro transactiekosten', 'degiro aansluitingskosten', 'externe kosten', 'stamp duty'}
